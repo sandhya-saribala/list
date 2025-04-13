4b6aa540dd4c4cc7b08253aba55f38c0587697cf
sandhya-saribala <saribalasandhya@gmail.com>
2025-04-13 21:48:27 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,15 @@
+#finding the max and min in nested list
+l=[[1,2,3],[4,5,6],[7,8,9]]
+final=[]
+for sublist in l:
+    largest=max(sublist)
+    final.append(largest)
+print(final)
+
+l=[[-1,-2,-3],[4,-6,3],[5,-6,-7]]
+final=[]
+for sublist in l:
+    smallest=min(sublist)
+    final.append(smallest) 
+print(final)
+
